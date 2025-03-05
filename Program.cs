e1acf1ed58efe69e59ab8e588814e46a83c485ab
serhat sakallı <sakalli0135@gmail.com>
2025-03-05 12:48:49 +0300

    aaa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ikilisayi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+          string input = Console.ReadLine();
+          string[] numbers=input.Split(' ');
+          int islem=0;
+          for (int i = 1; i < numbers.Length; i++)
+          {
+            if (int.Parse(numbers[i])==int.Parse(numbers[i-1]))
+                {islem=int.Parse(numbers[i]) +int.Parse( numbers[i-1]);Console.Write(" "+islem*islem);i++;}
+            else
+                {islem=int.Parse(numbers[i]) +int.Parse( numbers[i-1]);Console.Write(" "+islem);i++;}
+          }    
+        Console.ReadLine();
+    }
+}
